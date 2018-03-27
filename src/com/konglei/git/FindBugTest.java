1a6dd5944c288eb1455ece036bb0fbfed877db8b
13739224021 <37823048+13739224021@users.noreply.github.com>
2018-03-27 15:46:30 +0800

    Add files via upload

---
+++ src/com/konglei/git/FindBugTest.java
@@ -0,0 +1,15 @@
+package com.konglei.git;
+
+public class FindBugTest {
+private static String string;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				string=null;
+				if (string.equals("0")) {
+					System.out.println(string);
+System.out.println(string);
+
+				}
+	}
+
+}
